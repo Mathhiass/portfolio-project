f0f52df0cf84265a38a1405b628273b78cc954a5
Mathhiass <78061104+Mathhiass@users.noreply.github.com>
2024-05-21 21:26:01 +0800

    Update MenuOverlay.jsx

--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -6,7 +6,7 @@ const MenuOverlay = ({ links }) => {
     <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
         <li key={index}>
-          <NavLink href={link.path} title={link.title} />
+          <Navlink href={link.path} title={link.title} />
         </li>
       ))}
     </ul>
