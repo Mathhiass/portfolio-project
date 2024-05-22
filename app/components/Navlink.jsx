0386c7b8253cf4e761a808e16892f9e076811f8e
Mathhiass <78061104+Mathhiass@users.noreply.github.com>
2024-05-22 08:51:42 +0800

    not yet final

--- app/components/Navlink.jsx
+++ app/components/Navlink.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const Navlink = ({href, title}) => {
   return (
-    <Link href={href} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
+    <Link href={href} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white hover:underline">
         {title}
     </Link>
   );
