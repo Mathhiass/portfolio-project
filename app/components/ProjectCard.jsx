2e54d9c03e414172350ec527ed8e555a5a0e94dd
Mathhiass <78061104+Mathhiass@users.noreply.github.com>
2024-05-25 23:01:21 +0800

    Update ProjectCard.jsx

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -8,7 +8,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
     <div>
         <div className="h-52 md:h-72 rounded-t-xl relative group" 
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}>
-        </div>ProjectCard
+        </div>
         <div className="overlay item-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
           <Link
           href={gitUrl}
