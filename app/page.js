6bafcb101239781cadef09a8bd27db71148423cb
Mathhiass <78061104+Mathhiass@users.noreply.github.com>
2024-05-30 04:27:23 +0800

    EmailSection

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectSection";
+import EmailSection from "./components/EmailSection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
+        <EmailSection />
       </div>
     </main>
   );
